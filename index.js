cad1d2303f0c6cd15097a930f8cb521aac69520a
SangeethaShanmugm <sanjuriya22@gmail.com>
2024-05-12 10:19:41 +0530

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config()
+const cors = require("cors")
 const userRoute = require("./routes/userRoute")
 const itemRoute = require("./routes/itemRoute")
 const billRoute = require("./routes/billRoute")
@@ -10,6 +11,7 @@ const app = express();
 
 const PORT = 8000;
 app.use(express.json());
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("Welcome to Zepto management");
